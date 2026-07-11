75cfab2c4e981f92d9d797b7999fc65b40b932fd
Mahidhar_Reddy <mahidharreddy158@gmail.com>
2026-07-11 17:24:06 +0530

    Added user dashboard menu

--- MRS_no_db/main.py
+++ MRS_no_db/main.py
@@ -1,13 +1,45 @@
 from users import *
 
 def dashboard(user):
-    pass
+    while True:
+        print('Select a choice')
+        print('0. Logout')
+        print('1. Add a movie')
+        print('2. List all movies')
+        print('3. Add a review')
+        print('4. Edit a review')
+        print('5. Delete a review')
+
+        ch = input('Enter your choice (0 - 5) : ').strip()
+        if ch == '0':
+            print('You have logged out successfully...')
+            exit()
+
+        if ch == '1':
+            pass
+
+        elif ch == '2':
+            pass
+
+        elif ch == '3':
+            pass
+
+        elif ch == '4':
+            pass
+
+        elif ch == '5':
+            pass
+
+        else:
+            print('Invalid Choice. Please enter between (0 - 5)')
+        pass
+
 
 while True:
     print('0. exit')
     print('1. Register')
     print('2. Login')
-    c = input('Enter your choice (0/1/2): ')
+    c = input('Enter your choice (0/1/2): ').strip()
 
     if c == '0':
         print("Thanks for your interest in movies. Byee!!")
@@ -28,4 +60,4 @@ while True:
             print(user)
             dashboard(user)
         else:
-            print("Login failed", output[0])
+            print("Login failed!!", output)
