e89d4d874b9b98ddd7c9e975fafdac7a1fc8622b
Mahidhar_Reddy <mahidharreddy158@gmail.com>
2026-07-16 16:41:36 +0530

    added add_movie and view_user_reviews methods

--- MRS_no_db/movie_review_ms.py
+++ MRS_no_db/movie_review_ms.py
@@ -1,7 +1,14 @@
 from movies import *
+from reviews import *
 
 class MovieReviewSystem:
     def __init__(self, user):
         self.user = user
     
+    def add_a_movie(self, movie_title, movie_genre, movie_release_year):
+        add_movie(movie_title, movie_genre, movie_release_year)
     
+    def view_my_reviews(self):
+        get_user_reviews(self.user.get('id'))
+
+
