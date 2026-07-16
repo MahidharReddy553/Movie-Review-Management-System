ef2da14e41ce3df4daae633a27d0b198589cab17
Mahidhar_Reddy <mahidharreddy158@gmail.com>
2026-07-16 17:22:16 +0530

    Added add_review()

--- MRS_no_db/reviews.py
+++ MRS_no_db/reviews.py
@@ -7,4 +7,17 @@ def get_user_reviews(user_id):
     reviews = [r for r in data if r.get('user_id') == user_id]
     return reviews
 
+
+def add_review(u_id, m_id, m_rating, m_comment):
+    reviews = read_csv(path)
+
+    new_review = {
+        'id': generate_id(path),
+        'user_id': u_id,
+        'movie_id': m_id,
+        'rating': m_rating,
+        'comment': m_comment
+    }
+
+    write_row(path, new_review)
 # get_user_reviews('1')
